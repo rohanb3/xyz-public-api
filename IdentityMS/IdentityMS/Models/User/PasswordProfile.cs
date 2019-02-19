829a92649a955f7549d0c318d3ea7f97539c203a
andrii.matiushenko@ardas.dp.ua <andrii.matiushenko@ardas.dp.ua>
2019-02-19 10:42:45 +0200

    Remove old IdentityMS project, remove local git files

--- IdentityMS/IdentityMS/Models/User/PasswordProfile.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace graphApiService.Models.User
-{
-    public class PasswordProfile
-    {
-        public string Password { get; set; }
-        public bool? ForceChangePasswordNextLogin { get; set; }
-        public bool? EnforceChangePasswordPolicy { get; set; }
-    }
-}
