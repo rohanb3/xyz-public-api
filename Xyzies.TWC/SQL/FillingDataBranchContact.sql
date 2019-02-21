59cb22b09491ddaa4db273bc5966cbd83fcc99a6
Yurii.Kharlamov <yurij.kharlamov@ardas.dp.ua>
2019-02-21 11:45:10 +0200

    Scripts for filling branch tables

---
+++ Xyzies.TWC/SQL/FillingDataBranchContact.sql
@@ -0,0 +1,16 @@
+USE [TWC02122019]
+GO
+
+INSERT INTO [dbo].[TWC_BranchContact]([PersonName],[PersonLastName],[PersonTitle],[Value],[BranchContactTypeId],[BranchId])
+VALUES ('Charles','Polak','Test','9509940002',1,1)
+
+		   INSERT INTO [dbo].[TWC_BranchContact]([PersonName],[PersonLastName],[PersonTitle],[Value],[BranchContactTypeId],[BranchId])
+VALUES ('Lenaille Smith','Smith','Test','2205070082',1,1)
+
+		   INSERT INTO [dbo].[TWC_BranchContact]([PersonName],[PersonLastName],[PersonTitle],[Value],[BranchContactTypeId],[BranchId])
+VALUES ('Charles','Waddell','Test','elleyellc.lu@gmeil.com',2,3)
+
+		   INSERT INTO [dbo].[TWC_BranchContact]([PersonName],[PersonLastName],[PersonTitle],[Value],[BranchContactTypeId],[BranchId])
+VALUES ('Loc Nguyen','Nguyen','Test','elleyellc.lu@gmeil.com',3,3)
+
+GO
---
+++ Xyzies.TWC/SQL/FillingDataContactType.sql
@@ -0,0 +1,9 @@
+USE [TWC02122019]
+GO
+INSERT INTO [dbo].[TWC_BranchContactType]([Name]) VALUES('Phone')
+INSERT INTO [dbo].[TWC_BranchContactType]([Name]) VALUES('Email')
+INSERT INTO [dbo].[TWC_BranchContactType]([Name]) VALUES('Skype')
+INSERT INTO [dbo].[TWC_BranchContactType]([Name]) VALUES('Telegram')
+GO
+
+
