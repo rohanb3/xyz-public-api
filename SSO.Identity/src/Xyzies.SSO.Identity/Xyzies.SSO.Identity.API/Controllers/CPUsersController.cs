1c56d5de99c3817f7756b6cad32297a243725727
Yurii Platonov <yuriipl21@gmail.com>
2019-03-04 10:50:41 +0200

    implement getting user by permission (#11)

--- SSO.Identity/src/Xyzies.SSO.Identity/Xyzies.SSO.Identity.API/Controllers/CPUsersController.cs
+++ SSO.Identity/src/Xyzies.SSO.Identity/Xyzies.SSO.Identity.API/Controllers/CPUsersController.cs
@@ -1,13 +1,16 @@
 ﻿using System;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Xyzies.SSO.Identity.Data.Helpers;
 using Xyzies.SSO.Identity.Services.Service;
 
 namespace Xyzies.SSO.Identity.API.Controllers
 {
     [Route("api/cpusers")]
     [ApiController]
+    [Authorize]
     public class CpUsersController : ControllerBase
     {
         private readonly ICpUsersService _users;
@@ -20,15 +23,30 @@ namespace Xyzies.SSO.Identity.API.Controllers
         [HttpGet]
         public async Task<IActionResult> GetAllUsers()
         {
-            var users = await _users.GetAllCpUsers();
+            var role = HttpContext.User.Claims.FirstOrDefault(x => x.Type == Consts.RoleClaimType)?.Value;
+            var companyId = HttpContext.User.Claims.FirstOrDefault(x => x.Type == Consts.CompanyIdPropertyName)?.Value;
+
+            var users = await _users.GetAllCpUsers(role, int.Parse(companyId));
+            if (users == null)
+            {
+                return new ContentResult { StatusCode = 403 };
+            }
             return Ok(users);
         }
 
         [HttpGet("{id}")]
         public async Task<IActionResult> GetUserById(int id)
         {
-            var users = await _users.GetUserById(id);
-            return Ok(users);
+            var userId = int.Parse(HttpContext.User.Claims.FirstOrDefault(x => x.Type == Consts.UserIdPropertyName)?.Value);
+            var role = HttpContext.User.Claims.FirstOrDefault(x => x.Type == Consts.RoleClaimType)?.Value;
+            var companyId = HttpContext.User.Claims.FirstOrDefault(x => x.Type == Consts.CompanyIdPropertyName)?.Value;
+
+            var user = await _users.GetUserById(id, userId, role, int.Parse(companyId));
+            if (user == null)
+            {
+                return new ContentResult { StatusCode = 403 };
+            }
+            return Ok(user);
         }
     }
 }
--- SSO.Identity/src/Xyzies.SSO.Identity/Xyzies.SSO.Identity.Service/Service/CpUsersService.cs
+++ SSO.Identity/src/Xyzies.SSO.Identity/Xyzies.SSO.Identity.Service/Service/CpUsersService.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Text;
 using System.Threading.Tasks;
+using Xyzies.SSO.Identity.Data.Helpers;
 using Xyzies.SSO.Identity.Data.Repository;
 using Xyzies.SSO.Identity.Services.Models.User;
 
@@ -17,16 +18,41 @@ namespace Xyzies.SSO.Identity.Services.Service
             _cpUserRepo = cpUserRepo;
         }
 
-        public async Task<List<CpUser>> GetAllCpUsers()
+        public async Task<List<CpUser>> GetAllCpUsers(string authorRole, int companyId)
         {
-            var users = await _cpUserRepo.GetAsync();
-            return users.Adapt<List<CpUser>>();
+            if (authorRole == Consts.Roles.SuperAdmin)
+            {
+                var users = await _cpUserRepo.GetAsync();
+                return users.Adapt<List<CpUser>>();
+            }
+
+            if (authorRole == Consts.Roles.RetailerAdmin)
+            {
+                var companyUsers = await _cpUserRepo.GetByAsync(x => x.CompanyId == companyId);
+                return companyUsers.Adapt<List<CpUser>>();
+            }
+            return null;
         }
 
-        public async Task<CpUser> GetUserById(int id)
+        public async Task<CpUser> GetUserById(int id, int authorId, string authorRole, int companyId)
         {
-            var users = await _cpUserRepo.GetAsync(id);
-            return users.Adapt<CpUser>();
+            if (authorId != id && authorRole == Consts.Roles.SalesRep)
+            {
+                return null;
+            }
+
+            if (authorRole == Consts.Roles.SuperAdmin)
+            {
+                var user = await _cpUserRepo.GetAsync(id);
+                return user.Adapt<CpUser>();
+            }
+
+            if (authorRole == Consts.Roles.RetailerAdmin || authorRole == Consts.Roles.SalesRep)
+            {
+                var companyUser = await _cpUserRepo.GetByAsync(x => x.CompanyId == companyId && x.Id == id);
+                return companyUser.Adapt<CpUser>();
+            }
+            return null;
         }
     }
 }
