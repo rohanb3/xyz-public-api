edb61aaaab7e5f5a0998a25f13bf78ccaa2e56c8
andrii.matiushenko@ardas.dp.ua <andrii.matiushenko@ardas.dp.ua>
2019-05-08 10:08:35 +0300

    Add logger

---
+++ src/Xyzies.Logger/LoggerExtensions.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Serilog;
+using Serilog.Core;
+using Serilog.Events;
+using Serilog.Sinks.Network;
+using System;
+using Xyzies.Logger.Models;
+
+namespace Xyzies.Logger
+{
+    public static class LoggerExtensions
+    {
+        public static IServiceCollection AddTcpStreamLogging(this IServiceCollection services, Action<TcpLogOptions> setupOptions, LogEventLevel logLevel = LogEventLevel.Information, ILogEventEnricher[] enriches = null)
+        {
+            TcpLogOptions options = new TcpLogOptions();
+            setupOptions(options);
+
+            var protocol = options.SecureConnection ? "tls" : "tcp";
+            var uri = $"{protocol}://{options.Ip}:{options.Port}";
+
+            var loggerConfig = new LoggerConfiguration()
+                                .MinimumLevel.Is(logLevel)
+                                .WriteTo.TCPSink(uri);
+
+            if ((enriches?.Length ?? 0) > 0)
+            {
+                loggerConfig.Enrich.With(enriches);
+            }
+
+            Log.Logger = loggerConfig.CreateLogger();
+
+            services.AddLogging(loggingBuilder => loggingBuilder.AddSerilog(dispose: options.Dispose));
+
+            return services;
+        }
+    }
+}
---
+++ src/Xyzies.Logger/Models/TcpLogOptions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace Xyzies.Logger.Models
+{
+    public class TcpLogOptions
+    {
+        [DefaultValue(false)]
+        public bool SecureConnection { get; set; }
+        [DefaultValue(true)]
+        public bool Dispose { get; set; }
+        [Required]
+        public string Ip { get; set; }
+        [Required]
+        public int Port { get; set; }
+    }
+}
