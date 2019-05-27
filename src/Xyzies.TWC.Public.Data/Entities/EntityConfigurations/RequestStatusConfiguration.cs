0c0cc356df02d0639179127f86e729b291befc8d
andrii.matiushenko@ardas.dp.ua <andrii.matiushenko@ardas.dp.ua>
2019-05-27 18:47:37 +0300

    Add company status validation, add filter by status name

---
+++ src/Xyzies.TWC.Public.Data/Entities/EntityConfigurations/RequestStatusConfiguration.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Xyzies.TWC.Public.Data.Entities.EntityConfigurations
+{
+    public class RequestStatusConfiguration : IEntityTypeConfiguration<RequestStatus>
+    {
+        public void Configure(EntityTypeBuilder<RequestStatus> requestStatusBuilder)
+        {
+            requestStatusBuilder.ToTable("RequestStatus");
+
+        }
+    }
+}
