6387a8db728b01f9b289d13cb38cd2107efe06ee
Yurii Platonov <yuriipl21@gmail.com>
2019-11-14 17:58:59 +0200

    add field for settings

--- src/Xyzies.TWC.Public.Api/Models/TenantSettingModel.cs
+++ src/Xyzies.TWC.Public.Api/Models/TenantSettingModel.cs
@@ -2,8 +2,23 @@ namespace Xyzies.TWC.Public.Api.Models
 {
     public class TenantSettingModel
     {
-        public string Frst { get; set; }
-        public string Scnd { get; set; }
-        public string Thrd { get; set; }
+        public string PrimaryColor { get; set; }
+        public string SecondaryColor { get; set; }
+        public string LinkColor { get; set; }
+        public string Logo { get; set; }
+        public string Video { get; set; }
+        public string InputBackgroundColor { get; set; }
+        public string InputTextColor { get; set; }
+        public string ActiveInputBackgroundColor { get; set; }
+        public string ActiveInputTextColor { get; set; }
+        public string PlaceholderTextColor { get; set; }
+        public string PrimaryButtonColor { get; set; }
+        public string PrimaryButtonTextColor { get; set; }
+        public string TextColor { get; set; }
+        public bool ShowXYZLogo { get; set; }
+        public bool CallImage { get; set; }
+        public string DefaultButtonColor { get; set; }
+        public string DefaultButtonTextColor { get; set; }
+        public string IconColor { get; set; }
     }
 }
