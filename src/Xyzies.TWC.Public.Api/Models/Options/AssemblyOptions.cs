cf5f59bb480d271b8fe30c98d2d11db347b73f4e
artem.lupyr <artem.lupyr@ardas.biz>
2019-12-17 10:27:11 +0200

    Add Assembly version control

---
+++ src/Xyzies.TWC.Public.Api/Models/Options/AssemblyOptions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Xyzies.TWC.Public.Api.Models.Options
+{
+    public class AssemblyOptions
+    {
+        public string Name { get; set; }
+
+        public string Version { get; set; }
+
+        public string BuildNumber { get; set; }
+    }
+}
